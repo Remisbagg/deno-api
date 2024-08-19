11957c56e6dcdb746e34b3d40dd8f83fc317086a
Sergio <93742269+Remisbagg@users.noreply.github.com>
2024-08-19 12:29:52 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 // deps.ts
-export { Application, Router } from "https://deno.land/x/oak@v7.0.0/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
 export { Client } from "https://deno.land/x/mysql/mod.ts";
