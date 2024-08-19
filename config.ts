f321660508750d18d9e6396addecaa184d833de7
Sergio <93742269+Remisbagg@users.noreply.github.com>
2024-08-19 09:45:43 -0500

    changes

---
+++ config.ts
@@ -0,0 +1,9 @@
+// config.ts
+import { Client } from "./deps.ts";
+
+export const client = await new Client().connect({
+  hostname: "localhost",
+  username: "root",  
+  db: "baseDeno",
+  password: "", 
+});
