cb3bb809f54ea9c1a6b3b066412413c9dc9acd2b
Sergio <93742269+Remisbagg@users.noreply.github.com>
2024-08-19 12:13:56 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,2 @@
 // main.ts
 Deno.serve((req: Request) => new Response("Hello Worldd"));
-import { Application } from "./deps.ts";
-import router from "./routes.ts";
-
-const app = new Application();
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-console.log("Todo en orden YESSSSSSSSSSSSSSSSSS");
-const port = Number(Deno.env.get("PORT")) || 8000;
-await app.listen({ port });
